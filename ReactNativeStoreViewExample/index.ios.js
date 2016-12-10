61e265512b6729ef7378e4e7e1a926f81de4a04b
rh389 <roberthogan@blueyonder.co.uk>
2016-12-10 17:08:39 +0000

    Update example to RN 0.39

--- ReactNativeStoreViewExample/index.ios.js
+++ ReactNativeStoreViewExample/index.ios.js
@@ -1,7 +1,5 @@
-'use strict';
-
-import React, {Component} from "react";
-import {ActivityIndicator, AppRegistry, ScrollView, StyleSheet, Text, View, TouchableHighlight} from "react-native";
+import React, { Component } from "react";
+import { ActivityIndicator, AppRegistry, ScrollView, StyleSheet, Text, View, TouchableHighlight } from "react-native";
 import StoreViewManager from "react-native-store-view";
 
 type State = {
@@ -53,7 +51,7 @@ class ReactNativeStoreViewExample extends Component<void, void, State> {
   }
 
   onPressButton() {
-    this.setState({loading: true});
+    this.setState({ loading: true });
     StoreViewManager.isAvailable()
       .then(available => {
         if (!available) {
@@ -67,7 +65,7 @@ class ReactNativeStoreViewExample extends Component<void, void, State> {
         })
       })
       .then(() => {
-        this.setState({loading: false});
+        this.setState({ loading: false });
         console.log('SKStoreProductViewController successfully loaded the product over the net, but is not yet displaying anything');
         StoreViewManager.presentViewController();
       })
@@ -86,36 +84,35 @@ class ReactNativeStoreViewExample extends Component<void, void, State> {
   }
 }
 
-const
-  styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      fontSize: 15
-    },
-    button: {
-      marginTop: 50,
-      padding: 5,
-      borderWidth: 1,
-      borderColor: '#CCC',
-      borderRadius: 10,
-      height: 50,
-      width: 150
-    },
-    log: {
-      borderTopWidth: 1,
-      borderBottomWidth: 1,
-      borderColor: '#CCC',
-      padding: 5,
-      flex: 1,
-      marginBottom: 10
-    }
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    fontSize: 15
+  },
+  button: {
+    marginTop: 50,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#CCC',
+    borderRadius: 10,
+    height: 50,
+    width: 150
+  },
+  log: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#CCC',
+    padding: 5,
+    flex: 1,
+    marginBottom: 10
+  }
+});
 
 AppRegistry.registerComponent('ReactNativeStoreViewExample', () => ReactNativeStoreViewExample);
